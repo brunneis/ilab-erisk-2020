68b14f7c4adfd46ee73c8585f10aa998b0ac8910
Rodrigo Martínez <dev@brunneis.com>
2020-07-23 23:39:50 +0200

    Add download-models.sh

---
+++ datasets/download-datasets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://ipfs.io/ipfs/QmUmgNCjZdLidEEbSQ5BhBGr5wtrEF5JJgc3zRg9fs5s25 -O real-selfharmers-texts.csv
+wget https://ipfs.io/ipfs/QmcaLvBg1g2tjqKCd4f4W8jdRzqJtse9Wy1RahVmHKPJ6t -O users-texts-200k.csv
+wget https://ipfs.io/ipfs/QmWVU346JqpPg5L7YJxBB3NaJHjokyD1fQcqd5TzFejCpK -O users-texts-2m.csv
+wget https://ipfs.io/ipfs/QmW1bTBUkf56E9qgwUS99qoxJxvZAFJPdHgRTri6xip46N -O users-submissions-200k.csv
---
+++ models/download-models.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://ipfs.io/ipfs/QmQDaXmC7g7nneeEMqPz3s6LBBe6bjKXrdSDDzqzVGqoE6 -O xlmrb-selfharm-200k.tar && tar xf xlmrb-selfharm-200k.tar && rm -f xlmrb-selfharm-200k.tar
+wget https://ipfs.io/ipfs/QmYUADVhdENLmxY5YQPWrroTcZ1Wh7yBq5UANay48axSSk -O xlmrb-selfharm-2m.tar && tar xf xlmrb-selfharm-2m.tar && rm -f xlmrb-selfharm-2m.tar
+wget https://ipfs.io/ipfs/QmSqwYHYcN963FGf8iVRLgt9DAAaf6P4z59G9J4JXkAx65 -O xlmrb-selfharm-sub-200k.tar && tar xf xlmrb-selfharm-sub-200k.tar && rm -f xlmrb-selfharm-sub-200k.tar
